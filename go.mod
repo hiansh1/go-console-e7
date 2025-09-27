d725b54c6511aa32d0d7febbcb02ff714229c550
anas essalhi <anasessalhi18@gmail.com>
2025-09-27 17:24:57 +0100

    programme calls api from weither app

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hiansh1/go-console-e7.git
+
+go 1.24.6
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	apitest "github.com/hiansh1/go-console-e7.git/weither-api"
+)
 
 func main() {
-
-	fmt.Println("hello word")
-
+	var name string
+	fmt.Println("donner votre nom")
+	fmt.Scan(&name)
+	apitest.Say_Hello(name)
 }
